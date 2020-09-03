206e3e084c1a108254f680dd638619cb320757c0
Restyled.io <commits@restyled.io>
2020-09-03 14:01:42 +0000

    Restyled by clang-format

--- ios/Runner/GeneratedPluginRegistrant.h
+++ ios/Runner/GeneratedPluginRegistrant.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GeneratedPluginRegistrant : NSObject
-+ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry> *)registry;
 @end
 
 NS_ASSUME_NONNULL_END
